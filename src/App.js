51f1817c50954e38cfd509ca2ab8ae6f0ccbac13
Roman Orekhovskyi <orekhovskiy.roman@gmail.com>
2022-11-22 14:33:53 +0000

    Claculator alive

--- src/App.js
+++ src/App.js
@@ -3,100 +3,86 @@ import { Buttons, Calculator, Screen } from "./components";
 import "./styles.css";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [oper, setOper] = useState();
   const [result, setResult] = useState(0);
 
+  const clickNum = (i) => {
+    let click = i.target.value;
+    if (number === 0) {
+      setNumber(click);
+    } else {
+      setNumber(number + click);
+    }
+  };
+
   const ac = () => {
-    console.log("AC");
+    setNumber(0);
   };
+
   const change = () => {
-    console.log("+/-");
+    if (number > 0) {
+      setNumber(-number);
+    } else {
+      setNumber(Math.abs(number));
+    }
   };
-  const perc = () => {
-    console.log("%");
+
+  const perc = (i) => {
+    setNumber(number / 100);
   };
-  const divd = () => {
-    console.log("/");
-  };
-  const multiply = () => {
-    console.log("x");
-  };
-  const mins = () => {
-    console.log("-");
-  };
-  const pls = () => {
-    console.log("+");
-  };
+
   const eqls = () => {
-    console.log("=");
+    if (oper === "/") {
+      setNumber(parseFloat(result) / parseFloat(number));
+    }
+    if (oper === "x") {
+      setNumber(parseFloat(result) * parseFloat(number));
+    }
+    if (oper === "-") {
+      setNumber(parseFloat(result) - parseFloat(number));
+    }
+    if (oper === "+") {
+      setNumber(parseFloat(result) + parseFloat(number));
+    }
   };
-  const num1 = () => {
-    setResult(+1);
-    console.log("1");
-  };
-  const num2 = () => {
-    setResult(2);
-    console.log("2");
-  };
-  const num3 = () => {
-    setResult(3);
-    console.log("3");
-  };
-  const num4 = () => {
-    setResult(4);
-    console.log("4");
-  };
-  const num5 = () => {
-    setResult(5);
-    console.log("5");
-  };
-  const num6 = () => {
-    setResult(6);
-    console.log("6");
-  };
-  const num7 = () => {
-    setResult(7);
-    console.log("7");
-  };
-  const num8 = () => {
-    setResult(8);
-    console.log("8");
-  };
-  const num9 = () => {
-    setResult(9);
-    console.log("9");
-  };
-  const num0 = () => {
-    setResult(0);
-    console.log("0");
-  };
-  const point = () => {
-    setResult(".");
-    console.log(".");
+
+  const operator = (i) => {
+    let target = i.target.value;
+    setOper(target);
+    setResult(number);
+    setNumber(0);
   };
 
   return (
     <div className="App">
       <Calculator>
-        <Screen result={result} />
-        <Buttons value="AC" funcName={ac} idName="ac" />
-        <Buttons value="+/-" funcName={change} idName="changeSign" />
-        <Buttons value="%" funcName={perc} idName="percent" />
-        <Buttons value="/" funcName={divd} idName="divide" />
-        <Buttons value="x" funcName={multiply} idName="multiply" />
-        <Buttons value="-" funcName={mins} idName="minus" />
-        <Buttons value="+" funcName={pls} idName="plus" />
-        <Buttons value="=" funcName={eqls} idName="equals" />
-        <Buttons value="1" funcName={num1} idName="num1" />
-        <Buttons value="2" funcName={num2} idName="num2" />
-        <Buttons value="3" funcName={num3} idName="num3" />
-        <Buttons value="4" funcName={num4} idName="num4" />
-        <Buttons value="5" funcName={num5} idName="num5" />
-        <Buttons value="6" funcName={num6} idName="num6" />
-        <Buttons value="7" funcName={num7} idName="num7" />
-        <Buttons value="8" funcName={num8} idName="num8" />
-        <Buttons value="9" funcName={num9} idName="num9" />
-        <Buttons value="0" funcName={num0} idName="num0" />
-        <Buttons value="." funcName={point} idName="point" />
+        <Screen result={number} />
+        <Buttons sign="AC" funcName={ac} idName="ac" />
+        <Buttons sign="+/-" funcName={change} idName="changeSign" />
+        <Buttons sign="%" funcName={perc} idName="percent" />
+        <Buttons value={"/"} sign="/" funcName={operator} idName="divide" />
+        <Buttons value={"x"} sign="X" funcName={operator} idName="multiply" />
+        <Buttons value={"-"} sign="-" funcName={operator} idName="minus" />
+        <Buttons value={"+"} sign="+" funcName={operator} idName="plus" />
+        <Buttons value={"="} sign="=" funcName={eqls} idName="equals" />
+        <Buttons value={1} sign="1" funcName={clickNum} idName="num1" />
+        <Buttons value={2} sign="2" funcName={clickNum} idName="num2" />
+        <Buttons value={3} sign="3" funcName={clickNum} idName="num3" />
+        <Buttons value={4} sign="4" funcName={clickNum} idName="num4" />
+        <Buttons value={5} sign="5" funcName={clickNum} idName="num5" />
+        <Buttons value={6} sign="6" funcName={clickNum} idName="num6" />
+        <Buttons value={7} sign="7" funcName={clickNum} idName="num7" />
+        <Buttons value={8} sign="8" funcName={clickNum} idName="num8" />
+        <Buttons value={9} sign="9" funcName={clickNum} idName="num9" />
+        <Buttons
+          value={0}
+          sign="0"
+          funcName={clickNum}
+          idName="num0"
+          butId="but0"
+        />
+        <Buttons value={"."} sign="." fisignuncName={clickNum} idName="point" />
       </Calculator>
     </div>
   );
